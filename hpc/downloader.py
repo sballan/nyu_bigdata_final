c7f40589ff00a541cb3e8c2cbe1d1743c7b7bbfc
Samuel Ballan <sgb4622@gmail.com>
2021-08-27 17:55:18 -0400

    working with downloader

--- hpc/downloader.py
+++ hpc/downloader.py
@@ -15,37 +15,60 @@ API_QUANTITY = config['api_limit_quantity']
 DOWNLOADS_PATH = config['downloads_path']
 ENDPOINTS = config['glassnode_endpoints']
 
+print("Loaded Configs")
+
 # Start ray
 ray.init()
 
+@ray.remote
+def exec_request(req):
+  req = json.load(req)
+  endpoint = req['endpoint']
+  params = req['params']
+  coin = params['a']
 
-request_params = []
+  try:
+    r = requests.get(endpoint['url'],params=params)
+
+    with open(f"{DOWNLOADS_PATH}/{coin}/{endpoint['name']}.json",'w') as f:
+      f.write(r.text)
+    return [True, coin, endpoint['url']]
+  except:
+    return [False, coin, endpoint['url']]
+
+
+reqs = []
 # Next, iterate over endpoints and coins
 for coin in config['coins']:
-  for endpoint in endpoints:
-    params = endpoints['params']
+  for endpoint in ENDPOINTS:
+    params = endpoint['params']
     params['api_key'] = API_KEY
     params['a'] = coin
 
-    request_params.append(params)
+    req = {
+      "params": params,
+      "endpoint": endpoint
+    }
+
+    reqs.append(json.dumps(req))
+
+print(f"Preparing {len(reqs)} requests across {len(config['coins'])} coins and {len(ENDPOINTS)} endpoints")
 
 
-while len(request_params) > 0:
+while len(reqs) > 0:
+  num_reqs = min(len(reqs),API_QUANTITY)
+  print(f"Queueing up {num_reqs} requests at {time.time()}")
+
   start_time = time.time()
-
-  print(f"Queueing up {API_QUANTITY} requests at {time.time()}")
-
-  for i in range(API_QUANTITY):
-    # DO RAY
-
+  futures = [exec_request.remote(reqs.pop) for i in range(num_reqs)]
   time_elapsed = time.time() - start_time
+  print(f"We queued {num_reqs} in {time_elapsed} seconds.")
+  print(f"About to sleep for {API_RESET - time_elapsed} seconds.")
   time.sleep(API_RESET - time_elapsed)
 
+  print(ray.get(futures)) # [0, 1, 4, 9]
 
-r = requests.get(endpoint['url'],params=params)
 
-with open(f"test/{coin}/{endpoint['name']}.json",'w') as f:
-    f.write(r.text)
 
 
 
